c9f8a0f09952eacca17f9dd62e3e25a906781d47
daydy225 <dabelydadi@gmail.com>
2022-11-07 16:38:05 +0000

    3rd commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,14 @@ function App() {
 
   return (
     <div className="App">
-      <div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
+      </div> */}
       <h1>Vite + React by Daydy</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
